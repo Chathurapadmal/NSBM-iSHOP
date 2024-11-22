f50807355cdaa22bc6d75718364b1ec238613c3c
sar4nga <sar4nga@gmail.com>
2024-11-22 15:32:09 +0530

    footers added

--- admin/index.php
+++ admin/index.php
@@ -78,7 +78,7 @@ $categories_result = $conn->query($categories_query);
                     echo "<p>" . htmlspecialchars($product_row['description']) . "</p>";
                     echo "<p class='price'>Price: LKR " . htmlspecialchars($product_row['price']) . "</p>";
                     echo "</div>";
-                }
+                }5
             } else {
                 echo "<p>No products in this category.</p>";
             }
@@ -88,5 +88,74 @@ $categories_result = $conn->query($categories_query);
         echo "<p>No categories found.</p>";
     }
     ?>
+
+
+<!--botttom--> 
+   
+        
+<div class="bottomdown">
+        <table width="100%">
+            <tr >
+                <th>Store Location</th>
+                <th>Mail Us</th>
+                <th>Hotline</th>
+            </tr>
+            <tr>
+                <td>
+                    Mahenwaththa,<br>
+                    Pitipana,<br>
+                    Homagama, Sri Lanka.
+                </td>
+                <td>
+                   contact.nsbmishop@gmail.com
+                </td>
+                <td>
+                 0712365464
+                </td>
+            </tr> 
+            <tr>
+                <th>Categories</th>
+                <th class="middletc">Useful Links</th>
+                <th>Useful Link</th>
+                
+            </tr>
+            <tr>
+                <td>
+                    Iphones<br>
+                    MacBooks<br>
+                    IPads<br>
+                    Iwatch<br>
+                    Airpods<br>
+                </td>
+                <td class="middletc">
+                    Terms & Conditions<br>
+                        Privacy Policy<br>
+                        Delivery
+                </td>
+                <td>
+                    
+                        > Contact us<br>
+                        > Promotion
+                    
+                </td>
+              
+            </tr>
+     <tr><td></td>
+        <td><div class="smicons"> 
+            follow us:<br>
+            <a><img src="images/5296499_fb_facebook_facebook logo_icon.png"></a>
+            <a><img src="images/5296765_camera_instagram_instagram logo_icon.png"></a>
+            <a><img src="images/5296520_bubble_chat_mobile_whatsapp_whatsapp logo_icon.png"></a></div>
+        </td>
+    <td></td>
+    </tr></table>
+    <div class="footer"
+    <footer>
+
+    <p class="copyright">NSBM iSHOP All Rights Reserved</p></div></footer>
+    
+
+
+
 </body>
 </html>
